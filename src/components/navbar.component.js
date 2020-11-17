f529a9f97a08bb15b8d84a17c9f27a03458c9e5e
rtphan <rtphan@g.ucla.edu>
2020-11-17 06:15:16 +0000

    Finished tutorial.
    Created and connected exercise-list.component and edit-exercise-component and fixed typos.

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -10,7 +10,7 @@ export default class Navbar extends Component {
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                            <Link to="/" className="nav-link">Exercise</Link>
+                            <Link to="/" className="nav-link">Exercises</Link>
                         </li>
                         <li className="navbar-item">
                             <Link to="/create" className="nav-link">Create Exercise Log</Link>
